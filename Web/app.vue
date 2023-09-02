1dc18b609008d605b8c7088feb8e686e208260bc
Glenn Grainger <91460978+glenngrainger@users.noreply.github.com>
2023-09-02 22:53:13 +0100

    use scss, set colours, google font

--- Web/app.vue
+++ Web/app.vue
@@ -1,5 +1,18 @@
 <template>
+  <navbar>
+    <h2>Logo</h2>
+    <ul>
+      <li>Audio Converter</li>
+      <li>TODO!</li>
+    </ul>
+  </navbar>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style scoped lang="scss">
+navbar {
+  color: $secondary;
+}
+</style>
--- Web/nuxt.config.ts
+++ Web/nuxt.config.ts
@@ -1,4 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: "preconnect", href: "https://fonts.googleapis.com" },
+        {
+          rel: "preconnect",
+          crossorigin: "anonymous",
+          href: "https://fonts.gstatic.com",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&display=swap",
+        },
+      ],
+    },
+  },
+  css: ["~/assets/css/main.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/css/_colours.scss" as *;',
+        },
+      },
+    },
+  },
+});
