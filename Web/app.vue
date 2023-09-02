0f76c7981e4c9ddee65738e4fc81d4f6808b8b3d
Glenn Grainger <91460978+glenngrainger@users.noreply.github.com>
2023-09-02 23:03:58 +0100

    navbar styles

--- Web/app.vue
+++ Web/app.vue
@@ -1,18 +1,56 @@
 <template>
   <navbar>
-    <h2>Logo</h2>
+    <h2>Audio Converter</h2>
     <ul>
-      <li>Audio Converter</li>
-      <li>TODO!</li>
+      <li>
+        <a class="selected">Converter</a>
+      </li>
+      <li>
+        <a>Trim</a>
+      </li>
     </ul>
   </navbar>
-  <div>
+  <div class="main-content">
     <NuxtPage />
   </div>
 </template>
 
 <style scoped lang="scss">
 navbar {
-  color: $secondary;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 4rem;
+
+  h2 {
+    color: $secondary;
+  }
+
+  ul {
+    color: $primary;
+    font-weight: 500;
+    display: flex;
+    align-items: center;
+    gap: 1.5rem;
+
+    list-style-type: none;
+
+    a {
+      padding: 0.5rem 1rem;
+      border-radius: 6px;
+      cursor: pointer;
+      transition: background-color 0.25s ease-in-out;
+
+      &:hover {
+        background-color: $primaryVeryLight;
+      }
+
+      &.selected {
+        background-color: $primaryVeryLight;
+      }
+    }
+  }
+}
+.main-content {
+  padding: 0 4rem;
 }
 </style>
--- Web/pages/index.vue
+++ Web/pages/index.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>Index page</h1>
 </template>
+<style lang="scss" scoped></style>
